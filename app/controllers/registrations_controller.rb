4ef632ca9f053adb6104064019c1e8b3828a182f
miyazaki yuuya <yuya871207@gmail.com>
2017-01-10 11:05:55 +0000

    Finish user edit action

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,11 +3,14 @@ class RegistrationsController < Devise::RegistrationsController
   def after_sign_up_path_for(resource)
     user_path(resource)
   end
-
-
+ 
   private
-
+ 
       def sign_up_params
         params.require(:user).permit(:name, :email, :password, :password_confirmation)
+      end
+ 
+      def account_update_params
+        params.require(:user).permit(:name, :email, :password, :password_confirmation, :current_password)
       end
 end
