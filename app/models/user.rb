80547683daeeb3b20e86b4c46e36e00aaba35b6a
miyazaki yuuya <yuya871207@gmail.com>
2017-01-11 13:29:05 +0000

    Add user microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,10 @@ class User < ActiveRecord::Base
   has_many :microposts, dependent: :destroy
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+         
+  def feed
+    Micropost.where("user_id = ?", id)
+  end
   
   validates :name,  presence: true, length: { maximum: 50 }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ SampleApp::Application.routes.draw do
     :registrations => "registrations"
   }
   resources :users, only: [:show, :index, :destroy]
+  resources :microposts, only: [:create, :destroy]
   root "static_pages#home"
   match '/help', to: 'static_pages#help', via: 'get'
   match '/about', to: 'static_pages#about', via: 'get'
