d49702b9b4c3a8fa8799b04eab6a6e8b7eb93dd3
miyazaki yuuya <yuya871207@gmail.com>
2017-01-16 03:54:45 +0000

    Finish user following

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
          
   def feed
-    Micropost.where("user_id = ?", id)
+    Micropost.from_users_followed_by(self)
   end
   
   def following?(other_user)
